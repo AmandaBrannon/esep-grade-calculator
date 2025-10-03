1fd9d48b6d70b456d6b2312c8584b2466f21e221
AmandaBrannon <abrannon125@gmail.com>
2025-10-02 22:31:41 -0400

    Modified grade_calculator and grade_calculator_test
    I added a safeguard for dividing my zero in computeAverage, as well as adding a test that makes sure it returns an F (0).
    I added many tests to grade_calculator_test in order to reach 100% test coverage by testing the boundaries of the grades, testing an empty class with no grades added, tested for a ridiculously high score number (unnecessary due to already checking A), testing with multiple instantiations of assignments and exams, some as perfect duplicates just to see what could happen, and checked the GradeType function with its three options.

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -2,7 +2,7 @@ package esepunittests
 
 import "testing"
 
-func TestGetGradeA(t *testing.T) {
+func TestGetGradeAPerfect(t *testing.T) {
 	expected_value := "A"
 
 	gradeCalculator := NewGradeCalculator()
@@ -18,14 +18,14 @@ func TestGetGradeA(t *testing.T) {
 	}
 }
 
-func TestGetGradeB(t *testing.T) {
-	expected_value := "B"
+func TestGetGradeABoundary(t *testing.T) {
+	expected_value := "A"
 
 	gradeCalculator := NewGradeCalculator()
 
-	gradeCalculator.AddGrade("open source assignment", 80, Assignment)
-	gradeCalculator.AddGrade("exam 1", 81, Exam)
-	gradeCalculator.AddGrade("essay on ai ethics", 85, Essay)
+	gradeCalculator.AddGrade("open source assignment", 90, Assignment)
+	gradeCalculator.AddGrade("exam 1", 90, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 90, Essay)
 
 	actual_value := gradeCalculator.GetFinalGrade()
 
@@ -34,18 +34,125 @@ func TestGetGradeB(t *testing.T) {
 	}
 }
 
-func TestGetGradeF(t *testing.T) {
-	expected_value := "F"
+func TestGetGradeBBoundary(t *testing.T) {
+	expected_value := "B"
 
 	gradeCalculator := NewGradeCalculator()
 
-	gradeCalculator.AddGrade("open source assignment", 59, Assignment)
-	gradeCalculator.AddGrade("exam 1", 58, Exam)
+	gradeCalculator.AddGrade("open source assignment", 80, Assignment)
+	gradeCalculator.AddGrade("exam 1", 80, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 80, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeCBoundary(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 1", 70, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 70, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeDBoundary(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 60, Assignment)
+	gradeCalculator.AddGrade("exam 1", 60, Exam)
 	gradeCalculator.AddGrade("essay on ai ethics", 60, Essay)
 
 	actual_value := gradeCalculator.GetFinalGrade()
 
 	if expected_value != actual_value {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeFboundary(t *testing.T) {
+	expected_value := "F"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 59, Assignment)
+	gradeCalculator.AddGrade("exam 1", 59, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 59, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestEmptyCode(t *testing.T) {
+	expected_value := "F"
+
+	gradeCalculator := NewGradeCalculator()
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestExtraHigh(t *testing.T) {
+	expected_value := "A"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 1000, Assignment)
+	gradeCalculator.AddGrade("exam 1", 1001, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 999, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestManyAssignmentsAndDuplicates(t *testing.T) {
+	expected_value := "A"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
+	gradeCalculator.AddGrade("open source assignment", 92, Assignment)
+	gradeCalculator.AddGrade("open source assignment", 95, Assignment)
+	gradeCalculator.AddGrade("exam 1", 95, Exam)
+	gradeCalculator.AddGrade("exam 1", 95, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 100, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestTestingGradeTypeName(t *testing.T) {
+	if Assignment.String() != "assignment" {
+		t.Errorf("Expected 'assignment', got '%s'", Assignment.String())
+	}
+	if Exam.String() != "exam" {
+		t.Errorf("Expected 'exam', got '%s'", Exam.String())
+	}
+	if Essay.String() != "essay" {
+		t.Errorf("Expected 'essay', got '%s'", Essay.String())
 	}
 }
